c1f8b5fd8faaf0689fc49b0bc7569a48dd4365b2
kimdolion <kimowilkes@gmail.com>
2019-10-14 15:43:55 -0400

    Update home messages

--- src/components/General/Home.js
+++ src/components/General/Home.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const authenticatedOptions = (
   <React.Fragment>
-    <p>Check out the bubble tea collection so far by clicking on Orders
+    <p>Check out the bubble tea collection so far by clicking on Orders.
     </p>
-    <p>or get your collection going by clicking on Create Order.
+    <p>...Or get your collection going by clicking on Create Order.
     </p>
   </React.Fragment>
 )
@@ -19,7 +19,9 @@ const unauthenticatedOptions = (
 const Home = ({ user }) => (
   <React.Fragment>
     <h1>Welcome to Boba Tracker!</h1>
-    <p>Track your bubble tea orders so you can easily remember what you&apos;ve enjoyed in the past. You can also see what other people have ordered and try their boba order for yourself. If you do make sure to like that order by tapping the heart next to it!
+    <p>Track your bubble tea orders so you can easily remember what you&apos;ve enjoyed in the past.
+    </p>
+    <p>You can also see what other people have ordered and try their boba order for yourself. If you do make sure to like that order by tapping the heart next to it!
     </p>
     { user ? authenticatedOptions : unauthenticatedOptions }
   </React.Fragment>
