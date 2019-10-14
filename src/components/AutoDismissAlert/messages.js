d62eed45b40ceb62271e2f8e19f6ae96096526d3
kimdolion <kimowilkes@gmail.com>
2019-10-14 13:04:59 -0400

    Update alert messages
    - Added messaging for creating and updating orders
    - Styled the message to show up higher from the bottom

--- src/components/AutoDismissAlert/messages.js
+++ src/components/AutoDismissAlert/messages.js
@@ -5,5 +5,9 @@ export default {
   signInFailure: 'Failed to sign in. Check your email and password and try again.',
   signOutSuccess: 'Come back soon!',
   changePasswordSuccess: 'Password changed successfully!',
-  changePasswordFailure: 'Failed to change passwords. Check your old password and try again.'
+  changePasswordFailure: 'Failed to change passwords. Check your old password and try again.',
+  createSuccess: 'Successfully created an order!',
+  updateSuccess: 'You updated your order!',
+  deleteSuccess: 'You deleted an order!',
+  failure: 'Something went wrong! Please try again.'
 }
