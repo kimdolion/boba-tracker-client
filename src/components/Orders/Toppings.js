284bb474f0be0a6809ad4a42184bf9818788d754
kimdolion <kimowilkes@gmail.com>
2019-10-18 07:45:34 -0400

    Remove react-select

--- src/components/Orders/Toppings.js
+++ src/components/Orders/Toppings.js
@@ -1,17 +1,8 @@
 import React, { useState } from 'react'
-
-import CreatableSelect from 'react-select/creatable'
 import { toppingOptions } from './ToppingOptions'
 
 const Toppings = ({ order, handleChange }) => {
   const [selected] = useState([])
-  return (
-    <CreatableSelect
-      isMulti
-      selected={selected}
-      onChange={handleChange}
-      options={toppingOptions}
-    />
-  )
+  return selected
 }
 export default Toppings
