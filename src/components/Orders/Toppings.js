63d3957dba30d25ea88b55e21b8690688e8ef15c
kimdolion <kimowilkes@gmail.com>
2019-10-21 19:25:10 -0400

    Remove unnecessary component

--- src/components/Orders/Toppings.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react'
-import { toppingOptions } from './ToppingOptions'
-
-const Toppings = ({ order, handleChange }) => {
-  const [selected] = useState([])
-  return selected
-}
-export default Toppings
